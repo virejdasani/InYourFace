9bd1b910c2d8b6edf0affe6c1819d9c862408683
Virej Dasani <virej@Virejs-MacBook-Pro.local>
2022-05-19 12:57:22 +0530

    function that counts errors works\1

--- src/extension.ts
+++ src/extension.ts
@@ -509,6 +509,7 @@ export function activate(context: vscode.ExtensionContext) {
       _statusBarItem.show();
     }
     console.log("Errors: " + numErrors);
+    console.log(getNumErrors());
   }
 
   /**
@@ -605,6 +606,53 @@ function getNonce() {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
+}
+
+// function to get the number of errors in the open file
+function getNumErrors(): number {
+  const activeTextEditor: vscode.TextEditor | undefined =
+    vscode.window.activeTextEditor;
+  if (!activeTextEditor) {
+    return 0;
+  }
+  const document: vscode.TextDocument = activeTextEditor.document;
+
+  let numErrors = 0;
+  let numWarnings = 0;
+
+  let aggregatedDiagnostics: any = {};
+  let diagnostic: vscode.Diagnostic;
+
+  // Iterate over each diagnostic that VS Code has reported for this file. For each one, add to
+  // a list of objects, grouping together diagnostics which occur on a single line.
+  for (diagnostic of vscode.languages.getDiagnostics(document.uri)) {
+    let key = "line" + diagnostic.range.start.line;
+
+    if (aggregatedDiagnostics[key]) {
+      // Already added an object for this key, so augment the arrayDiagnostics[] array.
+      aggregatedDiagnostics[key].arrayDiagnostics.push(diagnostic);
+    } else {
+      // Create a new object for this key, specifying the line: and a arrayDiagnostics[] array
+      aggregatedDiagnostics[key] = {
+        line: diagnostic.range.start.line,
+        arrayDiagnostics: [diagnostic],
+      };
+    }
+
+    switch (diagnostic.severity) {
+      case 0:
+        numErrors += 1;
+        break;
+
+      case 1:
+        numWarnings += 1;
+        break;
+
+      // Ignore other severities.
+    }
+  }
+
+  return numErrors;
 }
 
 // this method is called when your extension is deactivated
